b92e807c8d303ea91b6bb5ed68928783e16a9e20
tedlopus <tedlopus@gmail.com>
2022-06-06 04:30:31 -0600

    testing heroku app failure

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/User');
 
 // http://localhost:3001/api/users
 // Create a new user
