76638778092e514c72b75612e45a71c5ada6990f
tedlopus <tedlopus@gmail.com>
2022-06-13 20:48:01 -0600

    updates

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,8 +2,19 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // http://localhost:3001/api/users
-// Create a new user
-router.post('/', async (req, res) => {
+// Get all users
+router.get('/', async(req,res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: {exclude: ['password']},
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+router.post('/signup', async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
@@ -14,7 +25,7 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json(err.message);
   }
 });
 
@@ -22,7 +33,7 @@ router.post('/', async (req, res) => {
 // Login the user
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
       res
@@ -48,7 +59,7 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json(err.message);
   }
 });
 
